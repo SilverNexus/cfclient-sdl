83b19d70a4d15f1fe373aac0a251c28bc5fa34b1
SilverNexus <hawkins280@proton.me>
2022-12-18 14:44:49 -0500

    Add header file needed for std::map

--- src/commands.cpp
+++ src/commands.cpp
@@ -10,6 +10,8 @@
  * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA 
  */
 
+#include <map>
+
 // Enumeration borrowed from the GTK code since it also fits exactly what we need.
 enum CmdFormat {
    ASCII,
